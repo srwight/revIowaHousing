7e90420c0ba7b55da9a36decbe4f8617a7bda8bc
Hanan Kwok <HananKwok532@users.noreply.github.com>
2020-01-03 14:47:11 -0500

    Added a docstring to kwokh.py
    The docstring has been added

--- kwokh.py
+++ kwokh.py
@@ -6,8 +6,12 @@ Created on Fri Jan  3 13:19:54 2020
 """
 
 def kwokh (a:int) -> str:
-    # Parameters: a should be a 1 or a 2
+    
+    """Parameters: a should be a 1 or a 2"""
+    
     if (a == 1):
         return "This is the 1st string."
-    else:
+    elif (a == 2):
         return "This is the 2nd string."
+    else:
+        return "You passed some other number %d." % a
