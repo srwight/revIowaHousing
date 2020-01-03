f722080af0534b165887f80d07d9980b3628deac
Ragygad <58750283+Ragygad@users.noreply.github.com>
2020-01-03 14:21:26 -0500

    Add files via upload

---
+++ ragyg.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  3 14:14:22 2020
+
+@author: rooga
+"""
+def ragyg(a:int) -> str:
+    """This is the docstring. In includes descriptive text about my function. 
+Parameters:
+-----------
+a should be a 1 or a 2
+"""
+    if(a=1):
+        return "This is my first string"
+    else:
+        return "This is my second string"
+    
