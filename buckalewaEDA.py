71b173408d9f0235fdcc8cb800f25133b8f8f19d
srwight <58702110+srwight@users.noreply.github.com>
2020-01-03 16:41:52 -0500

    Update buckalewaEDA.py

--- buckalewaEDA.py
+++ buckalewaEDA.py
@@ -4,7 +4,3 @@ def feature_extract(df:pd.DataFrame) -> pd.DataFrame:
     """Only extracts the ScreenPorch feature due to lack of correlation in other features."""
     features = df[['EnclosedPorch', 'ScreenPorch']]
     return features
-
-df = pd.read_csv('other/train.csv')
-
-print(feature_extract(df))
