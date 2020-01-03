e791439f4c9a54d9536e40a839467d9fb5b4583d
Hanan Kwok <HananKwok532@users.noreply.github.com>
2020-01-03 15:10:56 -0500

    Added my import & print statements
    import & print statements have been added

--- strings.py
+++ strings.py
@@ -8,7 +8,11 @@ Created on Fri Jan  3 13:12:23 2020
 
 #add an import string for your file.
 import wights
+import kwokh
 
 #Print the return of your function when passed a 1 and a 2:
 print(wights.wights(1))
 print(wights.wights(2))
+
+print(kwokh.kwokh(1)) # When a equals 1 
+print(kwokh.kwokh(2)) # When a equals 2
