99e505f2ab80c979880cab739fb08616db6e361d
srwight <58702110+srwight@users.noreply.github.com>
2020-01-03 13:16:29 -0500

    Add files via upload
    Initial string file committed to the master branch

---
+++ strings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  3 13:12:23 2020
+
+@author: srwight
+"""
+
+#add an import string for your file.
+
+#Print the return of your function when passed a 1 and a 2:
