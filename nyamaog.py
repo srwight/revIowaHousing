038dadb8acaeecdef3fbcbed8676a409e7a20fad
George Nyamao <gmnyamao@hotmail.com>
2020-01-03 14:21:00 -0500

    Add files via upload
    I have done it!

---
+++ nyamaog.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  3 14:07:49 2020
+
+@author: gmnya
+"""
+
+def nyamaog(a:int) -> str:
+    """This is the docstring. In includes descriptive text about my function. 
+Parameters:
+-----------
+a should be a 1 or a 2
+"""
+    a
+    if a == 1:
+        return "This is my first string"
+    elif a == 2:
+        return "My second one"
+    else:
+        return "I think you're lost! Go back!"
