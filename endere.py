90e6d257ec15d8b5b5cd4621fe8877d5cfabb6d5
Erik Ender <45837117+ErikEnder@users.noreply.github.com>
2020-01-03 14:09:03 -0500

    Add files via upload

--- endere.py
+++ endere.py
@@ -6,7 +6,15 @@ Created on Fri Jan  3 14:00:08 2020
 """
 
 def endere(a: int) -> str:
-    """ This function will return a different sentence depending on whether a is 1 or 2. """
+    """ This function will return a different sentence depending on whether a is 1 or 2. 
+    
+    Parameter - 
+    a: int
+    
+    Return - 
+    string
+    
+    """
     if (a == 1):
         return ("This is my first string.")
     else: 
