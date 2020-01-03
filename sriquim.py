97beaaa1e774128c6b2d9c8a9c722a0a927e2b61
sriqui23 <sriqui23@gmail.com>
2020-01-03 14:34:15 -0500

    sriquim2
    Here is the updated code

---
+++ sriquim.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  3 14:14:16 2020
+
+@author: sriqu
+"""
+
+def sriquim(a:int)->str:
+    """
+    a: int
+    1 or 2
+    if a is 1 return "You passed a 1"
+    if a is 2 return "you passed a 2"
+    """
+    if(a==1):
+        return "You passed a 1"
+    if(a==2):
+        return "You passed a 2"
+    else:
+        return "That is not a valid input"
+    
