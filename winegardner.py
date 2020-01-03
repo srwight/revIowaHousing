850a3d18800e789d1b4df2f8de390c0cfc9d4d62
jwine8 <56738179+jwine8@users.noreply.github.com>
2020-01-03 14:57:42 -0500

    Add files via upload

---
+++ winegardner.py
@@ -0,0 +1,17 @@
+
+
+#@author: winegardner
+""" """
+def Winegardner(a:int) -> str:
+   if (a ==1):
+        return "This is the 1st string."
+   elif (a==2):
+        return "This is the 2nd string."
+
+   
+
+    
+   
+
+
+
