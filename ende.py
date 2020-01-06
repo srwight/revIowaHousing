e422dbc5cd44cd6acb6feb57f9a416a3b90d05c2
Erik Ender <45837117+ErikEnder@users.noreply.github.com>
2020-01-06 09:49:20 -0500

    Updated ende file
    ende file now converts categorical values to numbered gradients rather than dummy variables.

--- ende.py
+++ ende.py
@@ -23,7 +23,7 @@ the new categorical features into it.
 
 """
 
-def eendreFeatures(df: pd.DataFrame) -> pd.DataFrame :
+def eenderFeatures(df: pd.DataFrame) -> pd.DataFrame :
     
     """
     
@@ -31,16 +31,52 @@ def eendreFeatures(df: pd.DataFrame) -> pd.DataFrame :
         df: pd.DataFrame = The entire Housing Prices DataFrame
         
     Returns:
-        pd.DataFrame = known as basement_dummies, returns dummy variables of the
-        defined features
+        pd.DataFrame = known as basement_data, returns numbered gradients of
+        previously String values in each column
         
     """
+    
+    # Fills nan values with NA in each column
+    df['BsmtCond'] = df['BsmtCond'].fillna('NA')
+    df['BsmtExposure'] = df['BsmtExposure'].fillna('NA')
+    df['BsmtFinType1'] = df['BsmtFinType1'].fillna('NA')
+    df['BsmtFinType2'] = df['BsmtFinType2'].fillna('NA')
     
     # Converting feature objects into strings
     df['BsmtCond'] = df['BsmtCond'].astype(str)
     df['BsmtExposure'] = df['BsmtExposure'].astype(str)
     df['BsmtFinType1'] = df['BsmtFinType1'].astype(str)
     df['BsmtFinType2'] = df['BsmtFinType2'].astype(str)
+    
+    
+    
+    condvars = ['NA','Po', 'Fa', 'TA', 'Gd', 'Ex'] # Map variables for BsmtCond
+    expvars = ['NA', 'No', 'Mn', 'Av', 'Gd'] # Map variables for BsmtExposure
+    finvars = ['NA', 'Unf', 'LwQ', 'Rec', 'BLQ', 'ALQ', 'GLQ'] # Map variables for BsmtFinType
+
+    condvals = list(range(6)) # List of values for BsmtCond
+    expvals = list(range(5)) # List of values for BsmtExp
+    finvals = list(range(7)) # List of values for BsmtFinType
+    
+    condmap = dict(zip(condvars, condvals)) # Map for BsmtCond
+    expmap = dict(zip(expvars, expvals)) # Map for BsmtExposure
+    finmap = dict(zip(finvars, finvals)) # Map for BsmtFinType
+
+    # Maps numbers 0 - 5 to values NA, Po, Fa, Ta, Gd, Ex respectively
+    newcol = df['BsmtCond'].apply(lambda x: condmap[x])
+    df['BsmtCond'] = newcol
+    
+    # Maps numbers 0 - 4 to values NA, No, Mn, Av, Gd respectively
+    newcol = df['BsmtExposure'].apply(lambda x: expmap[x])
+    df['BsmtExposure'] = newcol
+    
+    # Both FinType maps map numbers 0 - 6 to values NA, Unf, LwQ, Rec, BLQ, ALQ, GLQ respectively
+    newcol = df['BsmtFinType1'].apply(lambda x: finmap[x])
+    df['BsmtFinType1'] = newcol
+    
+    newcol = df['BsmtFinType2'].apply(lambda x: finmap[x])
+    df['BsmtFinType2'] = newcol
+    
     
     basement_data = pd.DataFrame()
     myFeatures = ['BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2']
@@ -49,14 +85,13 @@ def eendreFeatures(df: pd.DataFrame) -> pd.DataFrame :
     for feature in myFeatures:
         basement_data.insert(loc=0,column=feature,value=df[feature])
     
-    # Convert basement_data features into dummy variables
-    basement_dummies = pd.get_dummies(basement_data, drop_first = False)
-    return basement_dummies
+    return basement_data
+   
 
 # This section for testing purposes only
 def main():
     data = pd.read_csv(r"C:\Users\eende\Desktop\House Price Dataset\train.csv")
-    print(eendreFeatures(data))
+    print(eenderFeatures(data))
     
 if __name__ == '__main__':
     main()
