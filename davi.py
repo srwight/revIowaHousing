bcc48d99a024477d3aae48bdc247ab88857c0632
Matt Davis <37150230+Maraudier@users.noreply.github.com>
2020-01-06 13:24:45 -0500

    Added KitchenAbvGr back in
    Added KitchenAbvGr

--- davi.py
+++ davi.py
@@ -3,7 +3,7 @@ import pandas as pd
 def feature_extract(df:pd.DataFrame) -> pd.DataFrame:
       """Extracts 'Fireplaces', 'HalfBath', 'BedroomAbvGr', 'TotRmsAbvGrd' where 
       KitchenAbvGr is excluded due to its negative correlation to sale price being inversely related to proper sales price predictions"""
-    features = df[['Fireplaces', 'HalfBath', 'BedroomAbvGr', 'TotRmsAbvGrd']]
+    features = df[['Fireplaces', 'HalfBath', 'BedroomAbvGr', 'TotRmsAbvGrd', 'KitchenAbvGr']]
 
       """ Data Review
       Check for nan values = none
