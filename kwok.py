d81f0037714740dbd9702686255b997685c7997f
Hanan Kwok <HananKwok532@users.noreply.github.com>
2020-01-06 13:19:21 -0500

    Added code to handle skew
    There is now code to handle skew within the 4 features as well as fixes to some of the comments in the code.

--- kwok.py
+++ kwok.py
@@ -7,7 +7,7 @@ Created on Fri Jan  3 15:44:42 2020
 
 __doc__ = """
 
-Dependencies used (subject to change): pandas
+Dependencies used (subject to change): pandas, numpy, scipy.stats
 
 This .py file is meant to handle these 4 features/variables in feature engineering:
     GarageArea - the size/area of the garage in square feet
@@ -21,6 +21,8 @@ and possibly normalization/handling of skewed data.
 """
 
 import pandas as pd # import pandas in order to be able to bring in the dataset
+import numpy as np # import numpy to be able to take the log transformation
+from scipy.stats import skew # import skew to be able to handle skew 
 
 def feature_extract(dataset:pd.DataFrame) -> pd.DataFrame:
     """
@@ -51,8 +53,14 @@ def feature_extract(dataset:pd.DataFrame) -> pd.DataFrame:
     for c1 in myFeatures.columns.values:
         myFeatures = myFeatures.fillna(myFeatures[c1].mean())
         
-    # Handling outliers by replacing the values of those outliers
-    # with the value used to filter the data or with some value close to it
+    """
+    Handling outliers by replacing the values of those outliers
+    with the value used to filter the data or with some value close to it.
+    I looked at the scatterplots between each feature and SalePrice & 
+    estimated the max value of each feature(not including outliers).
+    These max values were what I used to determine the filtering values.
+    """
+        
     for ga in myFeatures['GarageArea']: # Garage area in square feet
         if (ga >= 1300):
             myFeatures['GarageArea'].replace(to_replace = ga, value = 1300)
@@ -69,12 +77,13 @@ def feature_extract(dataset:pd.DataFrame) -> pd.DataFrame:
         if (wd >= 800):
             myFeatures['WoodDeckSF'].replace(to_replace = wd, value = 790)
             
-    # Handle skewness and/or normalization (the next few lines of code are placeholders)
-    #print("GarageArea skew: %s\n" % myFeatures['GarageArea'].skew())
-    #print("GarageCars skew: %s\n" % myFeatures['GarageCars'].skew())
-    #print("OpenPorchSF skew: %s\n" % myFeatures['OpenPorchSF'].skew())
-    #print("WoodDeckSF skew: %s\n" % myFeatures['WoodDeckSF'].skew())     
-            
+    # Handle skewness and/or normalization
+    handle_skew = myFeatures.apply(skew)
+    handle_skew = handle_skew[handle_skew > 0.75]
+    loggedDF = np.log(myFeatures[handle_skew.index] + 1)
+    myFeatures = myFeatures.drop(handle_skew.index, axis = 1)
+    myFeatures = pd.concat([myFeatures, loggedDF], axis = 1)
+    
     # Returning the assigned features (still as a DataFrame) after processing them
     return myFeatures
 
@@ -82,7 +91,12 @@ def feature_extract(dataset:pd.DataFrame) -> pd.DataFrame:
 # This only runs when this .py file is the main file, not when it's imported.           
 def main():
     df = pd.read_csv("C:/Users/hanan/Desktop/BatchProject/Dataset/train.csv")
-    print(feature_extract(df))
+    extracted = feature_extract(df)
+    print("Extracted features: \n%s\n" % extracted)
+    print("GarageArea skew: %s\n" % extracted['GarageArea'].skew())
+    print("GarageCars skew: %s\n" % extracted['GarageCars'].skew())
+    print("OpenPorchSF skew: %s\n" % extracted['OpenPorchSF'].skew())
+    print("WoodDeckSF skew: %s\n" % extracted['WoodDeckSF'].skew())     
     
 if __name__ == '__main__':
     main()
