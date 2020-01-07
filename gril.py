25d6a5b0bd9c81fac3d766d1d8b3b2d2c68850ab
Stephen Wight <srwight@gmail.com>
2020-01-07 12:32:54 -0500

    Commit

--- gril.py
+++ gril.py
@@ -28,39 +28,19 @@ def feature_extract(traindata):
 
     #this gives me a std deviation of 23.994863
     #I filled in NaN values with the mean, 1978
-    # s: I think if we make a new dataframe object, we can do this like so:
-    yrmo = mydata[['YrSold','MoSold']]
-    yrmo.columns = ['Year','Month']
-    yrmo['Day'] = 1
-
-    # s: ok. I found it. But it's no good to have it as a datetime object.
-    # s: I'm going to divide it out a few times to get to seconds, then minutes
-    # s: then hours, then days. Eventually it's going to be normalized so
-    # s: I guess that doesn't matter too much, but hey.
-    datecol = pd.to_datetime(yrmo).astype(int)/10**9/60/60/24
-
-    # now to drop those columns
-    mydata.drop(['YrSold','MoSold'], axis = 1, inplace = True)
     
-
-    # I don't think any of the rest of this is necessary.
-
-    # for column in mydata:
-    #     newcol = mydata[column].apply(str)
-    #     mydata[column] = newcol
     
-    #Need to use Dummy variables since my data points are in months('MoSold') or in years(YrSold','YearBuilt','YearRemodAdd','GarageYrBlt')
-    # df_temp = pd.get_dummies(data=mydata,
-    #                        prefix=mydata.columns,
-    #                        prefix_sep= '_',
-    #                        dummy_na=False,)
+    # Need to use Dummy variables since my data points are in months('MoSold')
+    df_temp = pd.get_dummies(data=mydata.MoSold,
+                           prefix='MoSold',
+                           prefix_sep= '_',
+                           dummy_na=False)
 
-    # s: we need to build the names of the columns out.
-    
-    colnames = list(mydata.columns)
-    colnames.append('DateBuilt')
-    retdata = pd.concat([mydata, datecol], axis=1)
-    retdata.columns = colnames
+    mydata.drop('MoSold', axis=1)
+
+    # s: now we need to concatenate our dummies to our data
+
+    retdata = pd.concat([mydata, df_temp], axis=1)
     return retdata
 
 def main():
